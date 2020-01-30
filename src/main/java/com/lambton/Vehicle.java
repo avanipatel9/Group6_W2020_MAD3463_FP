19388006a4c1358fc42b6551ecf8fc8ca032a5a8
Avani Patel <anpatel36@yahoo.in>
2020-01-30 17:22:39 -0500

    [AP]Required variables declared in Vehicle class

--- src/main/java/com/lambton/Vehicle.java
+++ src/main/java/com/lambton/Vehicle.java
@@ -1,5 +1,16 @@
 package com.lambton;
  abstract class Vehicle
 {
+    String vehicleIdNumber;
+    String vehicleInfo;
+    String manufacturerName;
+    Boolean isSelfDrive;
+    String driver;
+    Boolean isInsured;
+    String insuranceProviderName;
+    int numberOfSeats;
+    String fuelType;
+    int ratePerDay;
+    int ratePerKM;
 
 }
