5383c4f312f6ec594352e751461f5712105e1487
Avani Patel <anpatel36@yahoo.in>
2020-01-30 17:34:38 -0500

    [AP]Getter Setter Methods created in Vehicle class

--- src/main/java/com/lambton/Vehicle.java
+++ src/main/java/com/lambton/Vehicle.java
@@ -11,6 +11,90 @@ package com.lambton;
     int numberOfSeats;
     String fuelType;
     int ratePerDay;
-    int ratePerKM;
+    double ratePerKM;
 
+    public String getVehicleIdNumber() {
+        return vehicleIdNumber;
+    }
+
+
+    public String getVehicleInfo() {
+        return vehicleInfo;
+    }
+
+    public void setVehicleInfo(String vehicleInfo) {
+        this.vehicleInfo = vehicleInfo;
+    }
+
+    public String getManufacturerName() {
+        return manufacturerName;
+    }
+
+    public void setManufacturerName(String manufacturerName) {
+        this.manufacturerName = manufacturerName;
+    }
+
+    public Boolean getSelfDrive() {
+        return isSelfDrive;
+    }
+
+    public void setSelfDrive(Boolean selfDrive) {
+        isSelfDrive = selfDrive;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public Boolean getInsured() {
+        return isInsured;
+    }
+
+    public void setInsured(Boolean insured) {
+        isInsured = insured;
+    }
+
+    public String getInsuranceProviderName() {
+        return insuranceProviderName;
+    }
+
+    public void setInsuranceProviderName(String insuranceProviderName) {
+        this.insuranceProviderName = insuranceProviderName;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public int getRatePerDay() {
+        return ratePerDay;
+    }
+
+    public void setRatePerDay(int ratePerDay) {
+        this.ratePerDay = ratePerDay;
+    }
+
+    public double getRatePerKM() {
+        return ratePerKM;
+    }
+
+    public void setRatePerKM(int ratePerKM) {
+        this.ratePerKM = ratePerKM;
+    }
 }
