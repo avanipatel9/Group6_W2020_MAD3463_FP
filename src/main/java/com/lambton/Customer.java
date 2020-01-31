4824047c2b9d1a8e4de979d5ec095c3aa63c2425
Charmi <c0768448@mylambton.ca>
2020-01-31 10:37:22 -0500

    [CP]Adding getter and setter Customer class

--- src/main/java/com/lambton/Customer.java
+++ src/main/java/com/lambton/Customer.java
@@ -7,4 +7,20 @@ public class Customer extends  Person
     String address;
     String city;
     ArrayList<VehicleRent> vehicleRents;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
