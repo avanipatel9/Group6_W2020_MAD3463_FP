d8e80108b295f51f65fff2ec2ae8f95689ebf3fb
SHREYA <shreyavaghasia99@gmail.com>
2020-01-31 16:44:05 -0500

    [SV] Addition of constructor in MotorCycle class

--- src/main/java/com/lambton/vehicle/MotorCycle.java
+++ src/main/java/com/lambton/vehicle/MotorCycle.java
@@ -5,6 +5,12 @@ public class MotorCycle
     int maxSpeed;
     float Milage;
 
+    public MotorCycle(int maxSpeed, float milage)
+    {
+        this.maxSpeed = maxSpeed;
+        Milage = milage;
+    }
+
     public int getMaxSpeed() {
         return maxSpeed;
     }
