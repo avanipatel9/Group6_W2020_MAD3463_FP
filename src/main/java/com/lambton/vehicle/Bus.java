0c2ca042e331806c4afab48e16e5a567dc021dfa
SHREYA <shreyavaghasia99@gmail.com>
2020-01-31 16:40:21 -0500

    [SV] Addition of getter and setter in bus class

--- src/main/java/com/lambton/vehicle/Bus.java
+++ src/main/java/com/lambton/vehicle/Bus.java
@@ -5,4 +5,28 @@ public class Bus
     String busType;
     boolean serviceAvailability;
     boolean wifiAvailability;
+
+    public String getBusType() {
+        return busType;
+    }
+
+    public void setBusType(String busType) {
+        this.busType = busType;
+    }
+
+    public boolean isServiceAvailability() {
+        return serviceAvailability;
+    }
+
+    public void setServiceAvailability(boolean serviceAvailability) {
+        this.serviceAvailability = serviceAvailability;
+    }
+
+    public boolean isWifiAvailability() {
+        return wifiAvailability;
+    }
+
+    public void setWifiAvailability(boolean wifiAvailability) {
+        this.wifiAvailability = wifiAvailability;
+    }
 }
