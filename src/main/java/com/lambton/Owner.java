05188180352a56f6fc18c87a975122c953cef639
Charmi <c0768448@mylambton.ca>
2020-01-31 10:45:52 -0500

    [CP]Adding getter and setter Owner class

--- src/main/java/com/lambton/Owner.java
+++ src/main/java/com/lambton/Owner.java
@@ -9,4 +9,27 @@ public class Owner extends  Person
     String website;
     List<Vehicle> vehicles;
 
+    public String getCompanyTitle() {
+        return companyTitle;
+    }
+
+    public void setCompanyTitle(String companyTitle) {
+        this.companyTitle = companyTitle;
+    }
+
+    public String getLandlineNumber() {
+        return landlineNumber;
+    }
+
+    public void setLandlineNumber(String landlineNumber) {
+        this.landlineNumber = landlineNumber;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
 }
