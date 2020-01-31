b26d944400343f5bf655bf6795693ca1e19d9de1
Charmi <c0768448@mylambton.ca>
2020-01-31 10:45:18 -0500

    [CP]Adding Owner class

---
+++ src/main/java/com/lambton/Owner.java
@@ -0,0 +1,12 @@
+package com.lambton;
+
+import java.util.List;
+
+public class Owner extends  Person
+{
+    String companyTitle;
+    String landlineNumber;
+    String website;
+    List<Vehicle> vehicles;
+
+}
