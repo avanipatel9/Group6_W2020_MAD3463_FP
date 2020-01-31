e2e6bbb74c9b902fdace155a4607e7994f214230
Charmi <c0768448@mylambton.ca>
2020-01-31 10:41:02 -0500

    [CP]Adding getter and setter Driver class

--- src/main/java/com/lambton/Driver.java
+++ src/main/java/com/lambton/Driver.java
@@ -6,5 +6,27 @@ public class Driver extends Person
     boolean isDrivingHistoryCleared;
     float salary;
 
+    public int getDrivingLicenceNumber() {
+        return drivingLicenceNumber;
+    }
 
+    public void setDrivingLicenceNumber(int drivingLicenceNumber) {
+        this.drivingLicenceNumber = drivingLicenceNumber;
+    }
+
+    public boolean isDrivingHistoryCleared() {
+        return isDrivingHistoryCleared;
+    }
+
+    public void setDrivingHistoryCleared(boolean drivingHistoryCleared) {
+        isDrivingHistoryCleared = drivingHistoryCleared;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
 }
