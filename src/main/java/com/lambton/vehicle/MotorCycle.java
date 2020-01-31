6edeed323b2d47c44d32fa4c2eee3baa3b75069f
SHREYA <shreyavaghasia99@gmail.com>
2020-01-31 16:41:12 -0500

    [SV] Addition of getter and setter in MotorCycle class

--- src/main/java/com/lambton/vehicle/MotorCycle.java
+++ src/main/java/com/lambton/vehicle/MotorCycle.java
@@ -4,4 +4,20 @@ public class MotorCycle
 {
     int maxSpeed;
     float Milage;
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public float getMilage() {
+        return Milage;
+    }
+
+    public void setMilage(float milage) {
+        Milage = milage;
+    }
 }
