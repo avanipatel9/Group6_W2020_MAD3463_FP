4181381d7f8ee2231cde3f2b23ae238073b91abe
Avani Patel <anpatel36@yahoo.in>
2020-02-02 18:36:14 -0500

    [AP] print method updated in Bus class

--- src/main/java/com/lambton/vehicle/Bus.java
+++ src/main/java/com/lambton/vehicle/Bus.java
@@ -38,7 +38,21 @@ public class Bus extends Vehicle
     }
 
     @Override
-    public void print() {
+    public void print()
+    {
+        System.out.println("Vehicle Identification Number : " + super.vehicleIdNumber);
+        System.out.println("Vehicle Information : " + super.vehicleInfo);
+        System.out.println("Manufacturer : " + super.manufacturerName);
+        System.out.println("Is insured : " + super.isInsured);
+        System.out.println("Insurence Provider : " + super.insuranceProviderName);
+        System.out.println("Vehicle Information : " + super.vehicleInfo);
+        System.out.println("No. of Seats : " + super.numberOfSeats);
+        System.out.println("Fuel Type : " + super.fuelType);
+        System.out.println("Rate Per Day : " + getRatePerDay());
+        System.out.println("Rate Per Kilometer : " + getRatePerKM());
+        System.out.println("Bus Type : " + busType);
+        System.out.println("WiFi : " + wifiAvailability);
+        System.out.println("Service availability : " + serviceAvailability);
 
     }
 }
