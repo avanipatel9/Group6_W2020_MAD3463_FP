9789e6666e34cebda860e4572a29f7e3efedf0b3
Avani Patel <anpatel36@yahoo.in>
2020-02-02 18:31:41 -0500

    [AP] print method updated in MotorCycle class

--- src/main/java/com/lambton/vehicle/MotorCycle.java
+++ src/main/java/com/lambton/vehicle/MotorCycle.java
@@ -3,12 +3,12 @@ package com.lambton.vehicle;
 public class MotorCycle extends Vehicle
 {
     int maxSpeed;
-    float Milage;
+    float milage;
 
     public MotorCycle(String vehicleIdNumber, String vehicleInfo, String manufacturerName, Boolean isSelfDrive, String driver, Boolean isInsured, String insuranceProviderName, int numberOfSeats, String fuelType, int ratePerDay, double ratePerKM, int maxSpeed, float milage) {
         super(vehicleIdNumber, vehicleInfo, manufacturerName, isSelfDrive, driver, isInsured, insuranceProviderName, numberOfSeats, fuelType, ratePerDay, ratePerKM);
         this.maxSpeed = maxSpeed;
-        Milage = milage;
+        milage = milage;
     }
 
     public int getMaxSpeed() {
@@ -20,15 +20,27 @@ public class MotorCycle extends Vehicle
     }
 
     public float getMilage() {
-        return Milage;
+        return milage;
     }
 
     public void setMilage(float milage) {
-        Milage = milage;
+        milage = milage;
     }
 
     @Override
-    public void print() {
-
+    public void print()
+    {
+        System.out.println("Vehicle Identification Number : " + super.vehicleIdNumber);
+        System.out.println("Vehicle Information : " + super.vehicleInfo);
+        System.out.println("Manufacturer : " + super.manufacturerName);
+        System.out.println("Is insured : " + super.isInsured);
+        System.out.println("Insurence Provider : " + super.insuranceProviderName);
+        System.out.println("Vehicle Information : " + super.vehicleInfo);
+        System.out.println("No. of Seats : " + super.numberOfSeats);
+        System.out.println("Fuel Type : " + super.fuelType);
+        System.out.println("Rate Per Day : " + getRatePerDay());
+        System.out.println("Rate Per Kilometer : " + getRatePerKM());
+        System.out.println("Maximum Speed : " + maxSpeed);
+        System.out.println(("Milage : " + milage));
     }
 }
