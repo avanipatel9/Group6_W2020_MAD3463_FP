9ef04b15d2c4ee0266aed9a1788dc6a557b90f05
Avani Patel <anpatel36@yahoo.in>
2020-02-01 23:37:17 -0500

    [AP] Class MotorCycle extends Vehicle class and modification in constructor

--- src/main/java/com/lambton/vehicle/MotorCycle.java
+++ src/main/java/com/lambton/vehicle/MotorCycle.java
@@ -1,12 +1,12 @@
 package com.lambton.vehicle;
 
-public class MotorCycle
+public class MotorCycle extends Vehicle
 {
     int maxSpeed;
     float Milage;
 
-    public MotorCycle(int maxSpeed, float milage)
-    {
+    public MotorCycle(String vehicleIdNumber, String vehicleInfo, String manufacturerName, Boolean isSelfDrive, String driver, Boolean isInsured, String insuranceProviderName, int numberOfSeats, String fuelType, int ratePerDay, double ratePerKM, int maxSpeed, float milage) {
+        super(vehicleIdNumber, vehicleInfo, manufacturerName, isSelfDrive, driver, isInsured, insuranceProviderName, numberOfSeats, fuelType, ratePerDay, ratePerKM);
         this.maxSpeed = maxSpeed;
         Milage = milage;
     }
