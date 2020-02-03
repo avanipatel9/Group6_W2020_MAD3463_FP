ff1fcf07501958670ce8ff95ed8d7c599a65afe0
Charmi <c0768448@mylambton.ca>
2020-02-02 22:23:55 -0500

    [CP]Adding URL validation function

--- src/main/java/com/lambton/Utilities.java
+++ src/main/java/com/lambton/Utilities.java
@@ -1,13 +1,28 @@
 package com.lambton;
+
+import java.net.URL;
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Utilities
 {
+    //Date Format Function
     public LocalDate getDateFromString(String date) throws ParseException {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         return LocalDate.parse(date, formatter);
+    }
+
+    //Website ULR Validation function
+    public  boolean isValid(String url)
+    {
+        try {
+            new URL(url).toURI();
+            return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
     }
 
 }
