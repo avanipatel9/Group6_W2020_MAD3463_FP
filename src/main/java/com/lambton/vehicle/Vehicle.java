debc8116801cebb14b66b09158f4a30ac00aaf55
Charmi <c0768448@mylambton.ca>
2020-02-03 16:35:32 -0500

    [CP]Vehicle Class Optimizing with comments and testing

--- src/main/java/com/lambton/vehicle/Vehicle.java
+++ src/main/java/com/lambton/vehicle/Vehicle.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 
 public abstract class Vehicle implements IDisplay
 {
+    //Vehicle Properties
     String vehicleIdNumber;
     String vehicleInfo;
     String manufacturerName;
