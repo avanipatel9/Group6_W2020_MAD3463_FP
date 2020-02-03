cc9b331d64aa905cbc017c7e5778858b183ee7f4
Charmi <c0768448@mylambton.ca>
2020-02-02 22:29:58 -0500

    [CP]Adding Contact Number validation function

--- src/main/java/com/lambton/Utilities.java
+++ src/main/java/com/lambton/Utilities.java
@@ -4,6 +4,8 @@ import java.net.URL;
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Utilities
 {
@@ -22,6 +24,21 @@ public class Utilities
         }
         catch (Exception e) {
             return false;
+        }
+    }
+
+    //Contact number Validation
+    public boolean isContactNumberValid(String number)
+    {
+        Pattern pattern = Pattern.compile("\\d{3}-\\d{7}");
+        Matcher matcher = pattern.matcher(number);
+
+        if (matcher.matches()) {
+            return  true;
+        }
+        else
+        {
+            return  false;
         }
     }
 
